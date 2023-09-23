66b48ae64e562c4d6212bca915e18af58a83ac34
Noam Bendelac <bendelac.noam@gmail.com>
2023-09-23 05:17:00 -0400

    Better classroom capstone description; link in piano

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -2,7 +2,7 @@
 import audioCapstone from '../images/spatial audio top view 4.png'
 import classroomCapstone from '../images/teacher captain1.png'
 import pianoVideo from '../images/Piano Demo.mov'
-import { Link } from './resume'
+import { Link, construct } from './resume'
 
 
 type Paragraph = (string | Link)[]
@@ -25,14 +25,14 @@ export const portfolio = [
     imageAlt: 'Screenshot of three dimensional physics diagram in web app',
     description: [
       Paragraph('I built a web app for interactive 3D diagrams of physics, mathematics, and other STEM subjects. I wanted these diagrams to provide a spatial, dynamic, and interactive demonstration of topics that would be harder to learn in a static or 2D diagram. I designed the 2D user interface in Figma and based the 3D diagrams on textbooks I had read in my studies.'),
-      Paragraph('The app also supports classroom use by letting teachers toggle between having control over students’ diagrams to guide their attention during an explanation, and letting students control their own diagrams independently to build intuition and familiarity. I implemented this multiplayer control using the WebSocket API.'),
+      Paragraph('The app also supports classroom use by letting teachers toggle between two modes: Captain Mode lets teachers control students’ diagrams to guide their attention during an explanation, and Explorer Mode lets students control their own diagrams independently to build intuition and familiarity. I implemented this multiplayer control using WebSocket connections.'), // "Captain Mode gives teachers control over students'..."?
     ],
   },
   {
     title: 'Piano Performance Visualization',
     video: 'https://www.youtube-nocookie.com/embed/C-eI5-nM-JE?si=XDQvKccZk1x7Sugx',
     description: [
-      Paragraph('I created a visualization of piano performance that highlights not just note pitch and duration, but also the performer’s dynamics (loudness) and tempo rubato (changes in speed).')
+      Paragraph('I created a visualization of piano performance that highlights not just note pitch and duration, but also the performer’s dynamics (loudness) and ', construct<Link>({ display: 'tempo rubato', href: 'https://en.wikipedia.org/wiki/Tempo_rubato'}), ' (changes in speed).')
     ]
   },
 ]
